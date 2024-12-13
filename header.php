24ff0f8fa82aca54775965852ab2481c4a4b7762
Nathanmarij <nathanmarijnissen@gmail.com>
2024-12-13 10:49:10 +0100

    Début du CSS

--- header.php
+++ header.php
@@ -1,14 +1,23 @@
 <header>
-    <div class="logo">
-        <img src="img/logo_conservatoire.svg" alt="Logo Conservatoire">
-    </div>
-    <nav class="itemNav">
-        <ul>
-            <li><a href="index.php" title="Accueil">Accueil</a></li>
-            <li><a href="event.php" title="Evénements">Evénements</a></li>
-            <li><a href="reservation.php" title="Mes réservations">Mes réservations</a></li>
-            <li><a href="compteUtilisateur.php" title="Compte">icone mon compte</a></li>
-        </ul>
-
+    <nav class="navbar navbar-expand-lg navbar-light container" style="background-color: #121D3F;">
+        <div class="d-flex w-100">
+            <a class="navbar-brand" href="index.php" title="Accueil">
+                <img src="img/logo_conservatoire.svg" alt="Logo Conservatoire" width="150" height="Auto" class="d-inline-block align-top">
+            </a>
+            <ul class="navbar-nav mx-auto align-self-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" title="accueil">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="event.php" title="Evénements">Evénements</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reservation.php" title="Mes réservations">Mes réservations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="compteUtilisateur.php" title="Compte">icone mon compte</a>
+                </li>    
+            </ul>
+        </div>
     </nav>
 </header>
