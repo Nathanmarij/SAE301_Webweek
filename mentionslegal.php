e3b5ab3577c3d7ad870098b425159b82ba929c94
ririne19 <marinehuard19@icloud.com>
2024-12-20 15:00:54 +0100

    Création page plan du site

--- mentionslegal.php
+++ mentionslegal.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="Mentions légales,Informations générales,Conditions générales d'utilisation, Propriété intellectuelle, Données personnelles, Droits des utilisateurs
-    Collecte des données">
-    <title>Mentions légales</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Réservation conservatoire du Puy en Velay - Mentions légales">
+    <title>Réservation conservatoire du Puy en Velay - Mentions légales</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
---
+++ plan_site.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Réservation conservatoire du Puy en Velay - Plan du site">
+    <title>Réservation conservatoire du Puy en Velay - Plan du site</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/mentions.css">
+
+</head>
+<header> <?php include("header.php"); ?></header>
+
+<body>
+    <main class="mentions">
+        <div class="container">
+            <h2 class="text-center text-uppercase fw-bold mb-4">Plan du Site</h2>
+            <article>
+                <div class="txt_article">
+                    <p>Voici le plan du site :</p>
+                    <p>Accueil</p>
+                    <p>Evénements</p>
+                    <p>Mes réservations</p>
+                    <ul>
+                        <li>Spectacle à venir</li>
+                        <li>Spectacle passé</li>
+                    </ul>
+                    <p>Mon compte</p>
+                </div>
+            </article>
+        </div>
+    </main>
+</body>
+<footer>
+    <?php include("footer.html"); ?>
+</footer>
+
+</html>
